d248d5844df1e035d82a02e6931dedcbcc6839de
Ndubuisi <ndujiovita1@gmail.com>
2025-12-29 07:39:26 +0100

    fix reentrancy vulnerability in contract

--- contracts/Reentrancy.sol
+++ contracts/Reentrancy.sol
@@ -15,9 +15,9 @@ contract EtherStore {
     function withdraw() external {
         uint256 balance = balances[msg.sender];
         require(balance > 0);
+        balances[msg.sender] = 0;
         (bool success, ) = msg.sender.call{value: balance}("");
         require(success, "Failed to send Ether");
-        balances[msg.sender] = 0;
     }
 
     // Helper function to check the balance of this contract
