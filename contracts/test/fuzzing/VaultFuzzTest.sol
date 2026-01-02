26e5c5042a61a4067700ec04b84e823b885f788e
Ndubuisi <ndujiovita1@gmail.com>
2026-01-02 10:54:47 +0100

    update vault test password

--- contracts/test/fuzzing/VaultFuzzTest.sol
+++ contracts/test/fuzzing/VaultFuzzTest.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.7;
 import "../../Vault.sol";
 
 contract VaultFuzzTest is Vault {
-    constructor() Vault("123asd123") {}
+    constructor() Vault("%#^hdgkse+jyy_jwyyy3j") {}
 
     function echidna_test_find_password() public view returns (bool) {
         return s_locked == true;
